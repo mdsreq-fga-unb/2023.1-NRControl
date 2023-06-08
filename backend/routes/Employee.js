7e9cec604621e3e5ab6aff32e7bbae1b9e73e311
juliaryoshida <juliayosh@gmail.com>
2023-06-07 22:34:17 -0300

    adding US06

---
+++ backend/routes/Employee.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const {Employee} = require("../models");
+
+router.get("/", async(req, res) => {
+    const listOfEmployees = await Employees.findAll();
+    res.json(listOfEmployees);
+})
+
+router.post("/", async(req, res) => {
+    const employees = req.body;
+    await Employee.create(employees);
+    res.json(employees);
+})
+
+
+module.exports = router;
+

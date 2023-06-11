7fdacd12931bca1c98f47a44be16ff77b995790d
Manoel Felipe <mftneto@gmial.com>
2023-06-11 14:45:35 -0300

    employee table

--- frontend2/src/pages/home/Home.js
+++ frontend2/src/pages/home/Home.js
@@ -1,10 +1,19 @@
 import React from "react";
 import axios from "axios";
+import "./Home.css";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {
+  BsFillPersonDashFill,
+  BsPersonFillGear,
+  BsFillPersonPlusFill,
+} from "react-icons/bs";
 
 function Home() {
   const navigateTo = useNavigate();
+  const goToRegister = () => {
+    navigateTo("/register");
+  };
 
   useEffect(() => {
     const accessToken = sessionStorage.getItem("accessToken");
@@ -16,12 +25,6 @@ function Home() {
 
   const [listOfEmployees, setListOfEmployees] = useState([]);
   useEffect(() => {
-      axios.get("http://localhost:3005/employeeinfo").then((response) => {
-        setListOfEmployees(response.data);
-      });
-  }, []);
-
-  useEffect(() => {
     axios.get("http://localhost:3005/employeeinfo").then((response) => {
       const sortedEmployees = response.data.sort((a, b) =>
         a.name.localeCompare(b.name)
@@ -31,17 +34,47 @@ function Home() {
   }, []);
 
   return (
-    <div>
-    <h1>Funcionários</h1>
-      {listOfEmployees.map((value, key) => {
-        return (
-          <div className="card" onClick={() => {navigateTo(`/employee/${value.id}`);}}>
-            <div className="name">Nome: {value.name}</div>
-            <div className="cpf">CPF: {value.cpf}</div>
-            <br/>
+    <div className="main">
+      <div className="header">
+        <h1>
+          Sonda <br></br>Engenharia
+        </h1>
+        <div className="icons-sidebar">
+          <BsFillPersonPlusFill onClick={goToRegister} />
+          <BsFillPersonDashFill />
+          <BsPersonFillGear />
+        </div>
+      </div>
+      <div className="box">
+        <h1>Funcionários</h1>
+        <div className="info">
+          <div className="tabel">
+            <table>
+              <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>CPF</th>
+                </tr>
+              </thead>
+              <tbody>
+                {listOfEmployees.map((value, key) => (
+                  <tr
+                    key={key}
+                    onClick={() => navigateTo(`/employee/${value.id}`)}
+                  >
+                    <td>
+                      <div className="name">{value.name}</div>
+                    </td>
+                    <td>
+                      <div className="cpf">{value.cpf}</div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
-        );
-      })}
+        </div>
+      </div>
     </div>
   );
 }
--- frontend2/src/pages/register/Register.js
+++ frontend2/src/pages/register/Register.js
@@ -10,13 +10,12 @@ import {
   BsPersonFillGear,
   BsFillPersonPlusFill,
 } from "react-icons/bs";
-import logo from "./../../assets/images/logo.png";
 
 const Register = () => {
   const navigateTo = useNavigate();
 
-  const goToRegister = () => {
-    navigateTo("/register");
+  const goToEmployees = () => {
+    navigateTo("/employees");
   };
 
   useEffect(() => {
@@ -76,16 +75,14 @@ const Register = () => {
   return (
     <div className="page">
       <div className="header">
-        <img src={logo} alt="logo" className="sonda" />
-        <h1>
+        <h1 onClick={goToEmployees}>
           Sonda <br /> Engenharia
         </h1>
         <div className="sidebar">
           <div className="icons-sidebar">
-            <BsFillPersonPlusFill onClick={goToRegister} />
+            <BsFillPersonPlusFill />
             <BsFillPersonDashFill />
             <BsPersonFillGear />
-            <BsPersonBadge />
           </div>
         </div>
       </div>
@@ -95,68 +92,66 @@ const Register = () => {
           <Formik
             initialValues={initialValues}
             onSubmit={onSubmit}
-            validationSchema={validationSchema}>
-
+            validationSchema={validationSchema}
+          >
             <Form className="formContainer">
               <div className="left-card">
-
                 <Field
                   id="inputCreatePost"
                   name="name"
-                  placeholder="Nome completo"/>
-                <ErrorMessage name="name" component="span"/>
+                  placeholder="Nome completo"
+                />
+                <ErrorMessage name="name" component="span" />
 
-                <Field 
-                  id="inputCreatePost" 
-                  name="cpf" 
-                  placeholder="CPF"/>
-                <ErrorMessage name="cpf" component="span"/>
+                <Field id="inputCreatePost" name="cpf" placeholder="CPF" />
+                <ErrorMessage name="cpf" component="span" />
 
-                <Field 
-                  id="inputCreatePost" 
-                  name="email" 
-                  placeholder="Email"/>
-                <ErrorMessage name="email" component="span"/>
+                <Field id="inputCreatePost" name="email" placeholder="Email" />
+                <ErrorMessage name="email" component="span" />
 
                 <Field
                   id="inputCreatePost"
                   name="address"
-                  placeholder="Endereço"/>
-                <ErrorMessage name="address" component="span"/>
-
+                  placeholder="Endereço"
+                />
+                <ErrorMessage name="address" component="span" />
               </div>
 
               <div className="right-card">
-                
                 <Field
                   id="inputCreatePost"
                   name="phonenumber"
-                  placeholder="Telefone"/>
-                <ErrorMessage name="phonenumber" component="span"/>
+                  placeholder="Telefone"
+                />
+                <ErrorMessage name="phonenumber" component="span" />
 
                 <Field
                   id="inputCreatePost"
                   name="birthday"
-                  placeholder="Data de nascimento"/>
-                <ErrorMessage name="birthday" component="span"/>
+                  placeholder="Data de nascimento"
+                />
+                <ErrorMessage name="birthday" component="span" />
 
                 <Field
                   id="inputCreatePost"
                   name="admissiondate"
-                  placeholder="Data de admissão"/>
-                <ErrorMessage name="admissiondate" component="span"/>
+                  placeholder="Data de admissão"
+                />
+                <ErrorMessage name="admissiondate" component="span" />
 
                 <Field
                   id="inputCreatePost"
                   name="asodate"
-                  placeholder="Data de ASO"/>
-                <ErrorMessage name="asodate" component="span"/>
-
+                  placeholder="Data de ASO"
+                />
+                <ErrorMessage name="asodate" component="span" />
               </div>
             </Form>
           </Formik>
         </div>
-        <button type="submit" className="cadastrar">Cadastrar</button>
+        <button type="submit" className="cadastrar">
+          Cadastrar
+        </button>
       </div>
     </div>
   );

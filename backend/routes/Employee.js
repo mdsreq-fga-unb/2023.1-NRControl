1f74e2b5df6034f3d9e490906541bbdad75d658c
juliaryoshida <juliayosh@gmail.com>
2023-06-11 22:21:18 -0300

    Adding error handling and fixing some bugs on yup validation

--- backend/routes/Employee.js
+++ backend/routes/Employee.js
@@ -1,23 +1,38 @@
 const express = require("express");
 const router = express.Router();
 const { Employee } = require("../models");
-const { validateToken } = require("../middlewares/auth");
 
 router.get("/", async (req, res) => {
-  const listOfEmployees = await Employee.findAll();
-  res.json(listOfEmployees);
+  try {
+    const listOfEmployees = await Employee.findAll();
+    res.json(listOfEmployees);
+  } catch (error) {
+    res.status(500).json({ error: "Falha ao obter lista de funcionários." });
+  }
 });
 
-router.get('/byId/:id', async (req, res) => {
+router.get("/byId/:id", async (req, res) => {
   const id = req.params.id;
-  const employee = await Employee.findByPk(id);
-  res.json(employee);
+  try {
+    const employee = await Employee.findByPk(id);
+    if (employee) {
+      res.json(employee);
+    } else {
+      res.status(404).json({ error: "Funcionário não encontrado." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Falha ao obter funcionário." });
+  }
 });
 
 router.post("/", async (req, res) => {
   const employees = req.body;
-  await Employee.create(employees);
-  res.json(employees);
+  try {
+    const createdEmployee = await Employee.create(employees);
+    res.status(201).json(createdEmployee);
+  } catch (error) {
+    res.status(500).json({ error: "Falha ao criar funcionário." });
+  }
 });
 
 module.exports = router;

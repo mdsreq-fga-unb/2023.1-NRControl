65eedd422baf5bbdcc95d9d8282b94ecd0917d4c
Júlia Yoshida <101576391+juliaryoshida@users.noreply.github.com>
2023-06-22 10:11:44 -0300

    Delete 2023.1-NRControl-login directory

--- 2023.1-NRControl-login/backend/views/routes/Password.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../../controllers/PasswordControllers");
-
-router.post("/", userController.resetPasswordRequest);
-router.get("/:id/:token", userController.validateResetPasswordUrl);
-router.post("/:id/:token", userController.resetPassword);
-
-module.exports = router;
--- 2023.1-NRControl-login/backend/views/routes/User.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../../controllers/UserControllers");
-
-router.post("/", userController.registerUser);
-router.post("/login", userController.loginUser);
-
-module.exports = router;

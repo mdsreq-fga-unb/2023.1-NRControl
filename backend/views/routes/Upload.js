5a26275dff72c070c90e715e0ea6ddd04438801c
juliaryoshida <juliayosh@gmail.com>
2023-06-29 18:48:31 -0300

    Adding US19

--- backend/views/routes/Upload.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const uploadController = require("../../controllers/UploadController");
-
-router.post("/upload", userController.registerUser);
-
-module.exports = router;

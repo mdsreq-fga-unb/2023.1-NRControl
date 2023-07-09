63d867b8d597371e8e707e7e463d4fbef5927f69
juliaryoshida <juliayosh@gmail.com>
2023-07-09 18:43:11 -0300

    Adding US18

--- backend/views/routes/Course.js
+++ backend/views/routes/Course.js
@@ -5,5 +5,6 @@ const courseController = require("../../controllers/CourseControllers");
 router.get("/", courseController.getCourses);
 router.post("/", courseController.createCourses);
 router.get("/:id", courseController.getCourseById);
+router.put("/:id", courseController.putCourse);
 
 module.exports = router;

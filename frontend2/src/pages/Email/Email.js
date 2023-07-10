1a72c09d101724a7ffe228459192ec484bb2e448
Manoel835 <mftneto@gmail.com>
2023-07-10 16:16:48 -0300

    Email

--- frontend2/src/pages/Email/Email.js
+++ frontend2/src/pages/Email/Email.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import logo from "./../../assets/images/logo.png";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -9,7 +10,7 @@ import "./Email.css";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#3f51b5",
+      main: "#ec5306",
     },
     secondary: {
       main: "#f50057",
@@ -55,68 +56,76 @@ function Email() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <form
-          onSubmit={handleSubmit}
-          className={`email-form ${sent ? "success" : ""}`}
-        >
-          <TextField
-            label="Destinatário"
-            value={to}
-            onChange={(e) => setTo(e.target.value)}
-            placeholder="Email de Destino"
-            fullWidth
-            required
-            InputLabelProps={{
-              style: { color: "#3f51b5" },
-            }}
-            InputProps={{
-              className: "textField toField",
-            }}
-          />
-
-          <TextField
-            label="Assunto"
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-            placeholder="Assunto do Email"
-            fullWidth
-            required
-            InputLabelProps={{
-              style: { color: "#3f51b5" },
-            }}
-            InputProps={{
-              className: "textField",
-            }}
-          />
-
-          <TextField
-            label="Mensagem"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            placeholder="Digite sua mensagem aqui..."
-            fullWidth
-            multiline
-            rows={6}
-            required
-            InputLabelProps={{
-              style: { color: "#3f51b5" },
-            }}
-            InputProps={{
-              className: "textField",
-            }}
-          />
-
-          <Button variant="contained" type="submit">
-            Enviar
-          </Button>
-        </form>
-
-        {error && <p>{error}</p>}
-        {sent && <p className="success-message">Email Enviado!</p>}
+    <div className="main-email">
+      <div className="logo">
+        <img src={logo} alt="SONDA Engenharia" className="sonda" />
       </div>
-    </ThemeProvider>
+      <div className="box-email">
+        <h1>Enviar Email</h1>
+        <ThemeProvider theme={theme}>
+          <div>
+            <form
+              onSubmit={handleSubmit}
+              className={`email-form ${sent ? "success" : ""}`}
+            >
+              <TextField
+                label="Destinatário"
+                value={to}
+                onChange={(e) => setTo(e.target.value)}
+                placeholder="Email de Destino"
+                fullWidth
+                required
+                InputLabelProps={{
+                  style: { color: "#f85605" },
+                }}
+                InputProps={{
+                  className: "textField toField",
+                }}
+              />
+
+              <TextField
+                label="Assunto"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+                placeholder="Assunto do Email"
+                fullWidth
+                required
+                InputLabelProps={{
+                  style: { color: "#f85605" },
+                }}
+                InputProps={{
+                  className: "textField",
+                }}
+              />
+
+              <TextField
+                label="Mensagem"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="Digite sua mensagem aqui..."
+                fullWidth
+                multiline
+                rows={6}
+                required
+                InputLabelProps={{
+                  style: { color: "#f85605" },
+                }}
+                InputProps={{
+                  className: "textField",
+                }}
+              />
+
+              <Button className="bnt-email" variant="contained" type="submit">
+                Enviar
+              </Button>
+            </form>
+
+            {error && <p>{error}</p>}
+            {sent && <p className="success-message">Email Enviado!</p>}
+          </div>
+        </ThemeProvider>
+      </div>
+    </div>
   );
 }
 

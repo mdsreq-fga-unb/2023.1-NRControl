803c51492398f4e93fe0059d09f4f1774b64695a
FelipeFigueiredo059 <felipefigueiredo043@gmail.com>
2023-07-16 18:54:07 -0300

    add: AWS-S3

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,32 @@ const passwordRoute = require("./views/routes/Password");
 const employeeRoute = require("./views/routes/Employee");
 const courseRoute = require("./views/routes/Course");
 const emailRoute = require("./views/routes/Email");
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
+const multer = require("multer");
+const { uuid } = require("uuidv4");
+const path = require("path");
 
 require("dotenv").config();
+
+aws.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.REGION,
+});
+
+const s3 = new aws.S3();
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: process.env.BUCKET_NAME,
+    acl: "public-read",
+    key(req, file, callback) {
+      callback(null, uuid() + path.extname(file.originalname));
+    },
+  }),
+});
 
 app.use(express.json());
 app.use(cors());
